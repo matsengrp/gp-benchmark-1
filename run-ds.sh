4ed92fc66db93caad514656e161b55f577683bb1
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2021-11-19 17:40:06 -0800

    edit run-ds for single tree plots

--- run-ds.sh
+++ run-ds.sh
@@ -16,13 +16,16 @@ cd _nograd_output
 
 gpb template --make-paths-absolute --mb basic.mb config.json run.mb
 
-for i in prep-gp.sh fit.sh outside.sh; do
+for i in prep-gp.sh prep-gp-singletree.sh fit.sh outside.sh single-tree-fit.sh; do
     gpb template --make-paths-absolute --mb $i config.json $i
 done
 
 #mb run.mb | tee mb.log
 bash prep-gp.sh
-bash fit.sh | tee fit.log
+bash prep-gp-singletree.sh
+declare -i NTREES=3 #$(wc -l < rerooted-topologies.noburnin.nonsingletons.nwk)
+# bash fit.sh | tee fit.log
+bash single-tree-fit.sh | tee single-tree-fit.log
 # bash outside.sh | tee outside.log
 
 # Now with gradients
@@ -30,13 +33,30 @@ cd ../_grad_output
 
 gpb template --make-paths-absolute --mb basic.mb config.json run.mb
 
-for i in prep-gp.sh fit.sh outside.sh; do
+for i in prep-gp.sh prep-gp-singletree.sh fit.sh outside.sh single-tree-fit.sh; do
     gpb template --make-paths-absolute --mb $i config.json $i
 done
 
 #mb run.mb | tee mb.log
 bash prep-gp.sh
-bash fit.sh | tee fit.log
+bash prep-gp-singletree.sh
+# bash fit.sh | tee fit.log
+bash single-tree-fit.sh | tee single-tree-fit.log
 # bash outside.sh | tee outside.log
+
+cd ..
+rm -rf _single_tree_plots
+mkdir _single_tree_plots
+
+declare -i count=0
+while [ $count -lt $NTREES ]; do
+	nograd_surf=_nograd_output/gp.$count.perpcsp_llh_surface.csv
+	nograd_track=_nograd_output/gp.$count.tracked_bl_correction.csv
+	grad_surf=_grad_output/gp.$count.perpcsp_llh_surface.csv
+	grad_track=_grad_output/gp.$count.tracked_bl_correction.csv
+	out_path=_single_tree_plots/$count.perpcsp_plot.pdf
+	gpb pcsptrackplot $nograd_surf $nograd_track $grad_surf $grad_track $out_path
+	count+=1
+done
 
 touch 0sentinel
